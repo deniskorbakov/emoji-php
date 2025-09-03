666b185eddd33f84550be14343903db0f9edcc9a
Denis <korbakovd@gmail.com>
2025-09-03 19:23:37 +0300

    fix: update logic generated smiles

--- src/Commands/EmojiGenerateCommand.php
+++ src/Commands/EmojiGenerateCommand.php
@@ -6,6 +6,7 @@ namespace DenisKorbakov\EmojiPhp\Commands;
 
 use DenisKorbakov\EmojiPhp\Commands\Arguments\ArgumentKey;
 use DenisKorbakov\EmojiPhp\Commands\Arguments\Arguments;
+use Throwable;
 
 final readonly class EmojiGenerateCommand implements Command
 {
@@ -20,6 +21,39 @@ final readonly class EmojiGenerateCommand implements Command
             ArgumentKey::LocaleKey
         );
 
-        echo $locale;
+        try {
+            $emojiFilepath = "vendor/milesj/emojibase/packages/data/$locale/compact.raw.json";
+            $cldrCodeFilePath = "vendor/milesj/emojibase/packages/data/en/shortcodes/cldr.raw.json";
+
+            if (! file_exists($emojiFilepath)) {
+                echo "Error: emoji with current locale is not exists\n";
+                exit(1);
+            }
+
+            if (! file_exists($cldrCodeFilePath)) {
+                echo "Error: short code with current locale is not exists\n";
+                exit(1);
+            }
+
+            $emojiFileData = file_get_contents($emojiFilepath, true);
+            $cldrFileData = file_get_contents($cldrCodeFilePath, true);
+
+            $emojis = json_decode($emojiFileData, true);
+            $cldrCodes = json_decode($cldrFileData, true);
+
+            foreach ($emojis as &$emoji) {
+                $hexCodeKey = $emoji['hexcode'];
+
+                $emoji['code'] = $cldrCodes[$hexCodeKey] ?? null;
+
+                unset($emoji['hexcode']);
+            }
+            unset($emoji);
+
+            file_put_contents("emojis/{$locale}.json", json_encode($emojis, JSON_PRETTY_PRINT));
+        } catch (Throwable $exception) {
+            echo "Error: {$exception->getMessage()}\n";
+            exit(1);
+        }
     }
 }
