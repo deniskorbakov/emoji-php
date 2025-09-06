f4545aa090209145709b4b128285c00aa1713897
Denis <korbakovd@gmail.com>
2025-09-07 01:27:22 +0300

    feat: add logic for make array with emoji

--- src/Parsers/EmojiListParser.php
+++ src/Parsers/EmojiListParser.php
@@ -4,17 +4,35 @@ declare(strict_types=1);
 
 namespace DenisKorbakov\EmojiPhp\Parsers;
 
+use DenisKorbakov\EmojiPhp\Files\Exceptions\FileNotFoundException;
 use DenisKorbakov\EmojiPhp\Files\FileJson;
 
 final class EmojiListParser implements Parser
 {
-    public function __construct(
-        public FileJson $emojisLocale,
-    ){
-    }
+	public const string SEPARATOR = ':';
 
-    public function parse(): array
-    {
-        return [];
-    }
+	public function __construct(
+		public FileJson $emojisLocale,
+	) {
+	}
+
+	/**
+	 * @return array<string, string>
+	 * @throws FileNotFoundException
+	 */
+	public function parse(): array
+	{
+		$emojisList = [];
+
+		$emojis = $this->emojisLocale->read();
+
+		foreach ($emojis as $emoji) {
+			$emojiUnicode = $emoji['unicode'];
+			$cldrCode = self::SEPARATOR . $emoji['code'] . self::SEPARATOR;
+
+			$emojisList[] = sprintf("'%s'=>'%s'", $emojiUnicode, $cldrCode);
+		}
+
+		return $emojisList;
+	}
 }
