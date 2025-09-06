ca68af5e90e8e3d0fc261e24c6d8f131d43ffcec
Denis <korbakovd@gmail.com>
2025-09-06 20:19:13 +0300

    feat: add new path

--- src/Files/EmojiFilePath.php
+++ src/Files/EmojiFilePath.php
@@ -6,31 +6,41 @@ namespace DenisKorbakov\EmojiPhp\Files;
 
 final readonly class EmojiFilePath
 {
-    public const string EMOJIS_DATA_BASE_PATH = 'vendor/milesj/emojibase/packages/data/';
+	public const string EMOJIS_DATA_BASE_PATH = 'vendor/milesj/emojibase/packages/data/';
 
-    public const string EMOJI_FILE_NAME = '/compact.raw.json';
-    public const string CLDR_CODE_FILE_NAME = 'en/shortcodes/cldr.raw.json';
+	public const string EMOJI_FILE_NAME = '/compact.raw.json';
+	public const string CLDR_CODE_FILE_NAME = 'en/shortcodes/cldr.raw.json';
 
-    public const string EMOJI_LOCALE_DIR = 'emojis/';
-    public const string EMOJI_LOCALE_EXTENSION = '.json';
+	public const string EMOJI_LOCALE_DIR = 'emojis/';
+	public const string EMOJI_LOCALE_EXTENSION = '.json';
 
-    public function __construct(
-        public string $locale,
-    ) {
-    }
+	public const string EMOJI_LIST_DIR = 'lists/';
+	public const string EMOJI_LIST_EXTENSION = '.php';
 
-    public function emoji(): string
-    {
-        return self::EMOJIS_DATA_BASE_PATH . $this->locale . self::EMOJI_FILE_NAME;
-    }
 
-    public function cldr(): string
-    {
-        return self::EMOJIS_DATA_BASE_PATH . self::CLDR_CODE_FILE_NAME;
-    }
+	public function __construct(
+		public string $locale,
+	) {
+	}
 
-    public function emojiLocale(): string
-    {
-        return self::EMOJI_LOCALE_DIR . $this->locale . self::EMOJI_LOCALE_EXTENSION;
-    }
+	public function emoji(): string
+	{
+		return self::EMOJIS_DATA_BASE_PATH . $this->locale . self::EMOJI_FILE_NAME;
+	}
+
+	public function cldr(): string
+	{
+		return self::EMOJIS_DATA_BASE_PATH . self::CLDR_CODE_FILE_NAME;
+	}
+
+	public function emojiLocale(): string
+	{
+		return self::EMOJI_LOCALE_DIR . $this->locale . self::EMOJI_LOCALE_EXTENSION;
+	}
+
+	public function list(): string
+	{
+		return self::EMOJI_LOCALE_DIR . self::EMOJI_LIST_DIR .
+			$this->locale . self::EMOJI_LIST_EXTENSION;
+	}
 }
