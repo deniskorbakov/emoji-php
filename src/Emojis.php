1e63d488c2afe6c989e4e9a19868336924b760fe
Denis <korbakovd@gmail.com>
2025-09-07 19:36:05 +0300

    feat: add serach method

--- src/Emojis.php
+++ src/Emojis.php
@@ -7,49 +7,77 @@ namespace DenisKorbakov\EmojiPhp;
 use DenisKorbakov\EmojiPhp\Files\EmojiFilePath;
 use DenisKorbakov\EmojiPhp\Files\Exceptions\FileNotFoundException;
 use DenisKorbakov\EmojiPhp\Files\File;
+use DenisKorbakov\EmojiPhp\Files\FileJson;
 use DenisKorbakov\EmojiPhp\Replacers\CodeToEmojiReplacer;
 use DenisKorbakov\EmojiPhp\Replacers\EmojiToCodeReplacer;
+use DenisKorbakov\EmojiPhp\Searches\EmojiSearch;
 
 final class Emojis
 {
-    public function __construct(
-        public Locale $locale,
-    ) {
-    }
+	public function __construct(
+		public Locale $locale,
+	) {
+	}
 
-    /**
-     * @throws FileNotFoundException
-     */
-    public function toEmoji(string $text): string
-    {
-        $emojiFile = new File(
-            new EmojiFilePath($this->locale)->list()
-        );
+	/**
+	 * Replace code on emoji unicode from text
+	 *
+	 * @throws FileNotFoundException
+	 */
+	public function toEmoji(string $text): string
+	{
+		$emojiFile = new File(
+			new EmojiFilePath($this->locale)->list()
+		);
 
-        return new CodeToEmojiReplacer($emojiFile, $text)->replace();
-    }
+		return new CodeToEmojiReplacer($emojiFile, $text)->replace();
+	}
 
-    /**
-     * @throws FileNotFoundException
-     */
-    public function toCode(string $text): string
-    {
-        $emojiFile = new File(
-            new EmojiFilePath($this->locale)->list()
-        );
+	/**
+	 * Replace emoji unicode on code from text
+	 *
+	 * @throws FileNotFoundException
+	 */
+	public function toCode(string $text): string
+	{
+		$emojiFile = new File(
+			new EmojiFilePath($this->locale)->list()
+		);
 
-        return new EmojiToCodeReplacer($emojiFile, $text)->replace();
-    }
+		return new EmojiToCodeReplacer($emojiFile, $text)->replace();
+	}
 
-    /**
-     * @return array<string, string>
-     * @throws FileNotFoundException
-     */
-    public function list(): array
-    {
-        /** @var array<string, string> */
-        return new File(
-            new EmojiFilePath($this->locale)->list()
-        )->execute();
-    }
+	/**
+	 * Get list emojis unicode with code group by category
+	 *
+	 * @return array<string, string>
+	 * @throws FileNotFoundException
+	 */
+	public function list(): array
+	{
+		/** @var array<string, string> */
+		return new File(
+			new EmojiFilePath($this->locale)->list()
+		)->execute();
+	}
+
+	/**
+	 * Search for emojis by tags based on locale
+	 *
+	 * Search works only from 2 characters
+	 *
+	 * @param string $text
+	 * @return array
+	 * @throws FileNotFoundException
+	 */
+	public function search(string $text): array
+	{
+		$emojiLocaleFile = new FileJson(
+			new File(
+				new EmojiFilePath($this->locale)->emojiLocale()
+			)
+		);
+
+		return new EmojiSearch($emojiLocaleFile, $text)->search();
+	}
 }
