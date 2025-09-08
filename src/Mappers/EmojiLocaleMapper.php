7da4b5c2c82799e6a225e2f674c1d75c79883859
Denis <korbakovd@gmail.com>
2025-09-08 18:20:59 +0300

    feat: add logic for make group

--- src/Mappers/EmojiLocaleMapper.php
+++ src/Mappers/EmojiLocaleMapper.php
@@ -10,27 +10,44 @@ use DenisKorbakov\EmojiPhp\Files\FileJson;
 final readonly class EmojiLocaleMapper implements Mapper
 {
     public function __construct(
-        public FileJson $sourceFile,
-        public FileJson $targetFile,
+        public FileJson $localeFile,
+        public FileJson $cldrFile,
+        public FileJson $groupsFile,
     ) {
     }
 
-	/**
-	 * @throws FileNotFoundException
-	 */
-	public function combine(): array
+    /**
+     * @throws FileNotFoundException
+     */
+    public function combine(): array
     {
-        $emojis = $this->sourceFile->read();
-        $cldrCodes = $this->targetFile->read();
+        $emojis = $this->localeFile->read();
+        $cldrCodes = $this->cldrFile->read();
+        $groups = $this->groupsFile->read();
 
-        foreach ($emojis as &$emoji) {
+        foreach ($emojis as $key => &$emoji) {
             $hexCodeKey = $emoji['hexcode'];
 
             $emoji['code'] = $cldrCodes[$hexCodeKey] ?? null;
 
             unset($emoji['hexcode']);
+
+            if (! array_key_exists('group', $emoji)) {
+                unset($emojis[$key]);
+                continue;
+            }
+
+            $groupNumber = $emoji['group'];
+
+            $emoji['group'] = $groups['groups'][$groupNumber]['message'] ?? null;
+
+            if (array_key_exists('order', $emoji)) {
+                unset($emoji['order']);
+            }
         }
 
-        return $emojis;
+        unset($emoji);
+
+        return array_values($emojis);
     }
 }
