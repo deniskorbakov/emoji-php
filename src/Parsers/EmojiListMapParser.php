f638e5354125f30ff001e86552439c7af8d0fd54
Denis <korbakovd@gmail.com>
2025-09-11 10:12:01 +0300

    feat: create list mapper

---
+++ src/Parsers/EmojiListMapParser.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DenisKorbakov\EmojiPhp\Parsers;
+
+final readonly class EmojiListMapParser implements Parser
+{
+	public const string SEPARATOR = ':';
+
+	/** @param array<int, array<string, mixed>> $emojisLocale */
+	public function __construct(
+		public array $emojisLocale,
+	) {
+	}
+
+	/** @return array<string, string> */
+	public function parse(): array
+	{
+		$emojisList = [];
+
+		foreach ($this->emojisLocale as $emoji) {
+			$emojiUnicode = $emoji['unicode'];
+			$cldrCode = self::SEPARATOR . $emoji['code'] . self::SEPARATOR;
+
+			$emojisList[$emojiUnicode] = $cldrCode;
+		}
+
+		return $emojisList;
+	}
+}
