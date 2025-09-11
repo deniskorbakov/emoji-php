15131e3fc84c220eb0ed9adead21511cee522bbf
Denis <korbakovd@gmail.com>
2025-09-11 18:16:58 +0300

    feat: create tests for file json

--- tests/Unit/Files/FileJson/ExistsTest.php
+++ tests/Unit/Files/FileJson/ExistsTest.php
@@ -1,3 +1,23 @@
 <?php
 
 declare(strict_types=1);
+
+
+use DenisKorbakov\EmojiPhp\Files\File;
+use DenisKorbakov\EmojiPhp\Files\FileJson;
+
+test('success - exists method delegates to file exists', function () {
+    $existingFile = tempnam(sys_get_temp_dir(), 'test') . '.json';
+    $nonExistingFile = '/non/existent/file.json';
+
+    $file1 = new File($existingFile);
+    $file1->save();
+
+    $fileJson1 = new FileJson($file1);
+    $fileJson2 = new FileJson(new File($nonExistingFile));
+
+    expect($fileJson1->exists())->toBeTrue()
+        ->and($fileJson2->exists())->toBeFalse();
+
+    unlink($existingFile);
+});
