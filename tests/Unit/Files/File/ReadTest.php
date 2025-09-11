0a2b628b653d1497d9c94265c87bd037c49f3110
Denis <korbakovd@gmail.com>
2025-09-11 18:11:21 +0300

    feat: create test for file

--- tests/Unit/Files/File/ReadTest.php
+++ tests/Unit/Files/File/ReadTest.php
@@ -1,3 +1,23 @@
 <?php
 
 declare(strict_types=1);
+
+use DenisKorbakov\EmojiPhp\Files\File;
+use DenisKorbakov\EmojiPhp\Files\Exceptions\FileNotFoundException;
+
+test('success - read method reads file content', function () {
+    $filename = tempnam(sys_get_temp_dir(), 'test');
+    file_put_contents($filename, 'test content');
+
+    $file = new File($filename);
+
+    expect($file->read())->toBe('test content');
+
+    unlink($filename);
+});
+
+test('fail - read method throws exception for non-existent file', function () {
+    $file = new File('/non/existent/file.txt');
+
+    $file->read();
+})->throws(FileNotFoundException::class);
--- tests/Unit/Files/File/SaveTest.php
+++ tests/Unit/Files/File/SaveTest.php
@@ -1,3 +1,21 @@
 <?php
 
 declare(strict_types=1);
+
+use DenisKorbakov\EmojiPhp\Files\File;
+
+test('success - save method creates empty file', function () {
+    $filename = sys_get_temp_dir() . '/test_save.txt';
+
+    if (file_exists($filename)) {
+        unlink($filename);
+    }
+
+    $file = new File($filename);
+    $file->save();
+
+    expect(file_exists($filename))->toBeTrue()
+        ->and(file_get_contents($filename))->toBe('');
+
+    unlink($filename);
+});
