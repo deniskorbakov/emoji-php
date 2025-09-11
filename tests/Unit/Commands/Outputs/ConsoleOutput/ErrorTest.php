13330e0e5759c5ad3959c6df3c60e7f7d9f24bfd
Denis <korbakovd@gmail.com>
2025-09-11 17:50:01 +0300

    feat: add test for console output

--- tests/Unit/Commands/Outputs/ConsoleOutput/ErrorTest.php
+++ tests/Unit/Commands/Outputs/ConsoleOutput/ErrorTest.php
@@ -1,3 +1,15 @@
 <?php
 
 declare(strict_types=1);
+
+use DenisKorbakov\EmojiPhp\Commands\Outputs\ConsoleOutput;
+
+test('success - echo error', function () {
+    $message = 'text error';
+
+    ob_start();
+    new ConsoleOutput($message)->error();
+    $output = ob_get_clean();
+
+    expect($output)->toContain('Error: ' . $message);
+});
