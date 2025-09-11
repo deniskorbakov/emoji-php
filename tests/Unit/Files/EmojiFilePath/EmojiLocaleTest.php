89a1db25640e36192fa004aca2bde527d5c62c96
Denis <korbakovd@gmail.com>
2025-09-11 18:05:39 +0300

    feat: create tests for emoji file path

--- tests/Unit/Files/EmojiFilePath/EmojiLocaleTest.php
+++ tests/Unit/Files/EmojiFilePath/EmojiLocaleTest.php
@@ -1,3 +1,12 @@
 <?php
 
 declare(strict_types=1);
+
+use DenisKorbakov\EmojiPhp\Files\EmojiFilePath;
+use DenisKorbakov\EmojiPhp\Locale;
+
+test('emojiLocale method returns correct path', function () {
+    $path = new EmojiFilePath(Locale::RU);
+
+    expect($path->emojiLocale())->toBe('emojis/locale/ru.json');
+});
--- tests/Unit/Files/EmojiFilePath/GroupsTest.php
+++ tests/Unit/Files/EmojiFilePath/GroupsTest.php
@@ -1,3 +1,12 @@
 <?php
 
 declare(strict_types=1);
+
+use DenisKorbakov\EmojiPhp\Files\EmojiFilePath;
+use DenisKorbakov\EmojiPhp\Locale;
+
+test('groups method returns correct path', function () {
+    $path = new EmojiFilePath(Locale::EN);
+
+    expect($path->groups())->toBe('vendor/milesj/emojibase/packages/data/en/messages.raw.json');
+});
