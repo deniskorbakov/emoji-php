2a6332941eaaee829ee882e50df6c15b1a8e9689
Denis <korbakovd@gmail.com>
2025-09-12 12:34:29 +0300

    fix: delete extra pkg

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Orchestra\Testbench\TestCase as BaseTestCase;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
